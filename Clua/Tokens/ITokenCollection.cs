fa45a49665b7d366b034d21cd748c65f9fb08f20
SebastianBathrick <sebastianbathrick@gmail.com>
2026-03-02 00:42:07 -0500

    Create ITokenCollection methods IsMatchAndConsumed & IsOfType

--- Clua/Tokens/ITokenCollection.cs
+++ Clua/Tokens/ITokenCollection.cs
@@ -2,7 +2,13 @@ namespace Clua.Tokens;
 
 interface ITokenCollection
 {
+    public bool IsEmpty { get; }
+    
     public void Add(Token token);
+    
+    public bool IsMatchAndConsumed(TokenType tokenType);
 
+    public bool IsOfType(TokenType tokenType);
+    
     public IReadOnlyList<Token> ToList();
 }
--- Clua/Tokens/TokenCollection.cs
+++ Clua/Tokens/TokenCollection.cs
@@ -3,16 +3,33 @@ namespace Clua.Tokens;
 class TokenCollection : ITokenCollection
 {
     readonly List<Token> _tokens = [];
+    int _index;
 
     public IReadOnlyList<Token> Tokens => _tokens;
 
     #region ITokenCollection Members
 
+    public bool IsEmpty => _index >= _tokens.Count;
+
     public void Add(Token token)
     {
         _tokens.Add(token);
     }
-    
+
+    public bool IsMatchAndConsumed(TokenType tokenType)
+    {
+        if (_index >= _tokens.Count || _tokens[_index].Type != tokenType)
+            return false;
+
+        _index++;
+        return true;
+    }
+
+    public bool IsOfType(TokenType tokenType)
+    {
+        return _index < _tokens.Count && _tokens[_index].Type == tokenType;
+    }
+
     public IReadOnlyList<Token> ToList()
     {
         // Ensure to copy the list to prevent external modification of the internal list
