7c09928d0eb7a81afbfd29a49764c991a32d6ad5
Sebastian Bathrick <72719337+SebastianBathrick@users.noreply.github.com>
2026-03-02 10:28:16 -0500

    Add CacheType XML docs

--- Clua/CodeGeneration/CacheType.cs
+++ Clua/CodeGeneration/CacheType.cs
@@ -1,7 +1,13 @@
 namespace Clua.CodeGeneration;
 
+/// <summary>
+/// The type of cache an instruction will access when executed by <see cref="VirtualMachine"/>.
+/// </summary>
 public enum CacheType
 {
+    /// <summary> No cache to access. </summary>
     None,
+    
+    /// <summary> Cache containing primitive literals. </summary>
     Constant
 }
