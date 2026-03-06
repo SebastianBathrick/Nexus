ec03d0ab4e187883387370cc45788a54ad7dff77
SebastianBathrick <sebastianbathrick@gmail.com>
2026-03-06 05:00:09 -0500

    Add basic CLI logic

---
+++ src/Nexus.Cli/Program.cs
@@ -0,0 +1,23 @@
+﻿using Nexus.Execution.Values;
+
+namespace Nexus.Cli;
+
+public static class Program
+{
+    const char ArgumentJoinSeperator = ' ';
+
+    public static int Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Error: No arguments provided");
+            return 1;
+        }
+        
+        var srcCode = String.Join(ArgumentJoinSeperator, args);
+        var result = NexusInterpreter.Run(srcCode);
+        
+        Console.WriteLine(result.ToString());
+        return result is NexusNumber ? result.ToInt() : 0;
+    }
+}
